371303e3b1beda8c967b47026f0cbd075eee6f11
Denislav <deni_9000_1990@abv.bg>
2022-04-13 19:27:05 +0300

    Settings heroku

--- ROBOWEB/settings.py
+++ ROBOWEB/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = DJANGO_APPS+PROJECT_APPS
 
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
